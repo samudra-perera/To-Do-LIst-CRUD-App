1ec7f1ba1e62e716829530a58e1965730289c020
Samudra Perera <samudrapup@gmail.com>
2022-08-15 03:29:47 -0400

    Working on the Delete Method

---
+++ public/main.js
@@ -0,0 +1,31 @@
+const deleteButton = document.querySelectorAll('.deleteButton')
+const completeButton = document.querySelectorAll('.completeButton')
+
+Array.from(deleteButton).forEach(elem => {
+    elem.addEventListener('click', deleteTask)
+})
+
+Array.from(completeButton).forEach(elem => {
+    elem.addEventListener('click', {
+        
+    })
+})
+
+async function deleteTask() {
+    const taskName = this.parentNode.childNodes[1].innerText
+    try {
+        const response = await fetch('deleteTask', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'taskNameS': taskName,
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    } catch (err) {
+        console.log(err)
+    }
+}
--- server.js
+++ server.js
@@ -1,40 +1,59 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const PORT = 3000;
 const MongoClient = require('mongodb').MongoClient
-const connectionString = 'mongodb+srv://ToDoList:1234567890@cluster0.m2ppucy.mongodb.net/?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://ToDoList:1234567890@cluster0.m2ppucy.mongodb.net/?retryWrites=true&w=majority';
+require('dotenv').config()
+
+let db
 
 MongoClient.connect(connectionString, {
     useUnifiedTopology: true })
     .then(client => {
+        db = client.db('to-do-list')
         console.log('Connected to the Database')
-        const db = client.db('to-do-list')
-        const toDoList = db.collection('toDoList')
-
-        app.use(bodyParser.urlencoded({ extended: true }))
-        app.set('view engine', 'ejs')
-
-        app.listen(3000, function() {
-            console.log('listening on 3000')
-        })
-
-        //Inserting a task into the to-do-list
-        app.post('/toDoList', (req, res) => {
-            toDoList.insertOne(req.body)
-            .then(result => {
-                res.redirect('/')
-            })
-            .catch(err => console.error(err))
-        })
-
-        //Getting the tasks from MongoDB 
-        app.get('/', (req, res) => {
-            db.collection('toDoList').find().toArray()
-            .then(results => {
-                res.render('index.ejs', { toDoList: results })
-            })
-            .catch(err => console.error(err))
-            
-        })
     })
     .catch(err => console.error(err))
+
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.set('view engine', 'ejs')
+
+    //Getting the tasks from MongoDB 
+    app.get('/', (req, res) => {
+        db.collection('toDoList').find().toArray()
+        .then(results => {
+            res.render('index.ejs', { toDoList: results })
+        })
+        .catch(err => console.error(err))
+        
+    })
+
+    //Inserting a task into the to-do-list
+    app.post('/toDoList', (req, res) => {
+        db.collection('toDoList').insertOne({
+            task: req.body.task,
+            taskType: req.body.taskType,
+            importance: req.body.importance,
+        })
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(err => console.error(err))
+    })
+
+    //Deleting a task from MongoDB 
+    app.delete('/deleteTask', (req, res) => {
+        console.log(req)
+        db.collection('toDoList').deleteOne({task: req.body.taskNameS})
+        .then(result => {
+            console.log('Task got deleted')
+            res.json('Task got Deleted')
+        })
+        .catch(err => console.error(err))
+    })
+    
+    app.listen(process.env.PORT || PORT, ()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
